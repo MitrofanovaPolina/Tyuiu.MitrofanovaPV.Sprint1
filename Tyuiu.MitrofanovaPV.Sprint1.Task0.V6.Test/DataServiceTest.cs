53fa6c7086743f8981057a0208bc90b3bfcbaa30
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-09-30 22:32:34 +0500

    Add project files.

---
+++ Tyuiu.MitrofanovaPV.Sprint1.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint1.Task0.V6.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint1.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(15, res);
+        }
+    }
+}
