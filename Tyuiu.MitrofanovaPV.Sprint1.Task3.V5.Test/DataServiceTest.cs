84156a454f9a44283ea8d016f2748c34e797e0a5
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-01 02:25:29 +0500

    Task3

---
+++ Tyuiu.MitrofanovaPV.Sprint1.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint1.Task3.V5.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint1.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 120;
+            double y = 3.5;
+            var res = ds.DistanceLength(x, y);
+            Assert.AreEqual(420, res);
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint1.Task3.V5/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint1.Task3.V5.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint1.Task3.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Митрофанова П.В. | ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #5                                                             *");
+            Console.WriteLine("* Выполнила: Митрофанова Полина Викторовна | ПИНб-25-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x, y;
+            Console.WriteLine("введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.DistanceLength(x, y));
+            Console.ReadKey();
+        }
+    }
+}
