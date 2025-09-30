0391374af93814ddbbb74ad2508e82ce1d259abf
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-01 03:07:22 +0500

    Task4

---
+++ Tyuiu.MitrofanovaPV.Sprint1.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint1.Task4.V1.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint1.Task4.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(0.04, res);
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint1.Task4.V1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint1.Task4.V1.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint1.Task4.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Митрофанова П.В. | ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнила: Митрофанова Полина Викторовна | ПИНб-25-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x;
+            Console.WriteLine("введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("1 / (x + 2)^2 =" + ds.Calculate(x));
+            Console.ReadKey();
+        }
+    }
+}
