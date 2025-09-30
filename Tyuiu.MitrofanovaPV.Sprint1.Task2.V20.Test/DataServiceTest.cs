f18a0500447f166f964a1ac62cf4af7a97b7b4a2
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-01 01:47:37 +0500

    Task2

---
+++ Tyuiu.MitrofanovaPV.Sprint1.Task2.V20.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint1.Task2.V20.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint1.Task2.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int y = 3;
+            var res = ds.CalculateSquaresSumm(x, y);
+            Assert.AreEqual(13, res);
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint1.Task2.V20/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint1.Task2.V20.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint1.Task2.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Митрофанова П.В. | ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнила: Митрофанова Полина Викторовна | ПИНб-25-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x, y;
+            Console.WriteLine("введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.CalculateSquaresSumm(x, y));
+            Console.ReadLine();
+        }
+    }
+}
