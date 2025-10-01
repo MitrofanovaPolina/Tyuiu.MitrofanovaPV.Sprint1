6761dc6177c9521d8e9443bf53cda709b3b786ec
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-01 06:03:29 +0500

    Task7

---
+++ Tyuiu.MitrofanovaPV.Sprint1.Task7.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint1.Task7.V22.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint1.Task7.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 45;
+            double y = 45;
+            double z = 1;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(z, res);
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint1.Task7.V22/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint1.Task7.V22.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint1.Task7.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Митрофанова П.В. | ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнила: Митрофанова Полина Викторовна | ПИНб-25-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* исходным значениям данных, вводимых пользователем                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("                ctg x");
+            Console.WriteLine(" z = (1 - tan x)      + cos (x - y)");
+            double x, y;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x,y));
+            Console.ReadLine();
+        }
+    }
+}
